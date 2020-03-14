abe1343dec21916f26de3ddc020fcf5e0d37d23e
K0nek0 <56084697+K0nek0@users.noreply.github.com>
2020-03-14 15:50:38 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -17,6 +17,4 @@ L = m*l**2*v_phi(t)**2/2 + m*R*l*omega**2*cos(phi(t)-omega*t) + m*g*l*cos(phi(t)
 
 print(diff(L, phi(t)))
 print()
-print(diff(L, v_phi(t)))
-print()
 print(diff(diff(L, v_phi(t)), t))
